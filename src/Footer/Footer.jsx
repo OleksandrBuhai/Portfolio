0d329f539ce8fd74f7add24861b9c6f6e0b3eb97
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-06-16 19:04:56 +0200

    little reorganize , added some fetch

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import containerStyle from '../common/styles/Container.module.css'
+import {Link} from "react-scroll";
 
 function Footer () {
+
+
     return (
         <div className={styles.footerContainer}>
             <div className={`${containerStyle.container} ${styles.footersBlock}`}>
                 <footer className={styles.footer}>
                     Copyryting ©: 2023 by Oleksandr Buhai | All Rights Reserver.
                 </footer>
-                <button>Up</button>
+                <div className={styles.goUp}><Link to="main" smooth={true}>UP</Link></div>
             </div>
         </div>
     )
--- src/Main/Main/Main.jsx
+++ src/Main/Main/Main.jsx
@@ -3,6 +3,10 @@ import style from "../Main/Main.module.css"
 import containerStyle from "../../common/styles/Container.module.css"
 import {motion} from "framer-motion";
 import portfolio from"../../common/img/photo/portfolio.jpeg"
+import styles from "../../Footer/Footer.module.css";
+import GitHub from "../../common/img/socialneb/assets/github.png";
+import Instagram from "../../common/img/socialneb/assets/instagram.png";
+import LinkeId from "../../common/img/socialneb/assets/linkedin.png";
 
 
 function Main() {
@@ -17,6 +21,11 @@ function Main() {
                               <span>Hello </span>
                               <h1>I am Oleksandr Buhai</h1>
                               <p>And I'm a Frontend Developer</p>
+                            <div className={style.socialLinks}>
+                                <img src={GitHub} alt=""/>
+                                <img src={Instagram} alt=""/>
+                                <img src={LinkeId} alt=""/>
+                            </div>
                         </div>
                         <div className={style.photo}>
                             <motion.div
